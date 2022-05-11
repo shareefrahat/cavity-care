898c78a5544a2f80bcc9f909c4b43188ef17ce7c
ShareefRahat <imr4h47@gmail.com>
2022-05-11 11:57:40 +0600

    first commit with react router  tailwind and daisyui setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import About from "./pages/About/About";
+import Appointment from "./pages/Appointment/Appointment";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Reviews from "./pages/Reviews/Reviews";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route
+          path="/appointment"
+          element={<Appointment></Appointment>}
+        ></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }

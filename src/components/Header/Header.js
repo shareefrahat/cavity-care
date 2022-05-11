1a4c31d56421686f9f230a1b794a6c765020de3d
ShareefRahat <imr4h47@gmail.com>
2022-05-11 12:06:55 +0600

    header ui updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,42 +11,60 @@ const Header = () => {
       <li>
         <Link to="/appointment">Appointment</Link>
       </li>
+
+      <li>
+        <Link to="/reviews">Reviews</Link>
+      </li>
+
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
     </>
   );
   return (
     <>
-      <div class="navbar bg-base-100 flex flex-row justify-between">
-        <div class="navbar-start flex flex-row justify-between  items-center lg:justify-start ">
-          <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
+      <div className="navbar bg-base-100 flex flex-row justify-between">
+        <div className="navbar-start flex flex-row justify-between  items-center lg:justify-start ">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
+                className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
             </label>
             <ul
-              tabindex="0"
-              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               {menu}
             </ul>
           </div>
-          <Link to="/" class="btn btn-ghost normal-case text-xl text-primary">
+          <Link
+            to="/"
+            className="btn btn-ghost normal-case text-xl text-neutral"
+          >
             Cavity Care
           </Link>
         </div>
-        <div class="navbar-center hidden lg:flex">
-          <ul class="menu menu-horizontal p-0">{menu}</ul>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">{menu}</ul>
         </div>
       </div>
     </>

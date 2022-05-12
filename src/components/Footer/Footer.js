0e6636002c996e64aa49fc6ba8e26e1cabe7d4ee
ShareefRahat <imr4h47@gmail.com>
2022-05-12 07:56:04 +0600

    footer link and bg added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import bgFooter from "../../images/footer.png";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <>
+      <footer
+        style={{ backgroundImage: `url(${bgFooter})` }}
+        className="
+      text-neutral bg-no-repeat bg-cover bg-center mx-auto"
+      >
+        <div className="footer flex-col lg:flex-row justify-center lg:justify-around my-5">
+          <div>
+            <span className="footer-title">Services</span>
+            <Link to="/" className="link link-hover">
+              Branding
+            </Link>
+            <Link to="/" className="link link-hover">
+              Design
+            </Link>
+            <Link to="/" className="link link-hover">
+              Marketing
+            </Link>
+            <Link to="/" className="link link-hover">
+              Advertisement
+            </Link>
+          </div>
+          <div>
+            <span className="footer-title">Company</span>
+            <Link to="/" className="link link-hover">
+              About us
+            </Link>
+            <Link to="/" className="link link-hover">
+              Contact
+            </Link>
+            <Link to="/" className="link link-hover">
+              Jobs
+            </Link>
+            <Link to="/" className="link link-hover">
+              Press kit
+            </Link>
+          </div>
+          <div>
+            <span className="footer-title">Legal</span>
+            <Link to="/" className="link link-hover">
+              Terms of use
+            </Link>
+            <Link to="/" className="link link-hover">
+              Privacy policy
+            </Link>
+            <Link to="/" className="link link-hover">
+              Cookie policy
+            </Link>
+          </div>
+        </div>
+        <div className="mt-20 py-5">
+          <p>Copyright © 2022 - All right reserved by Cavity Care</p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;

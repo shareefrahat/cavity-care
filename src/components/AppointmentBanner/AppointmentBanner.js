f94ac044eb17585be5a8ce5331a944384a88d7a8
ShareefRahat <imr4h47@gmail.com>
2022-05-13 19:11:28 +0600

    calender color updated

--- src/components/AppointmentBanner/AppointmentBanner.js
+++ src/components/AppointmentBanner/AppointmentBanner.js
@@ -3,6 +3,21 @@ import { DayPicker } from "react-day-picker";
 import chair from "../../images/chair.png";
 
 const AppointmentBanner = ({ date, setDate }) => {
+  const css = `
+  .my-selected:not([disabled]) { 
+    font-weight: bold; 
+    border: 2px solid #0FCFEC;
+  }
+  .my-selected:hover:not([disabled]) { 
+    border-color: #0FCFEC;
+    color: #19D3AE;
+  }
+  .my-today { 
+    font-weight: bold;
+    font-size: 140%; 
+    color: #0FCFEC;
+  }
+`;
   return (
     <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,7 +29,16 @@ const AppointmentBanner = ({ date, setDate }) => {
           />
         </div>
         <div className="flex flex-col lg:items-start text-left w-[375px] lg:w-[600px]">
-          <DayPicker mode="single" selected={date} onSelect={setDate} />
+          <style>{css}</style>
+          <DayPicker
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            modifiersClassNames={{
+              selected: "my-selected",
+              today: "my-today",
+            }}
+          />
         </div>
       </div>
     </div>

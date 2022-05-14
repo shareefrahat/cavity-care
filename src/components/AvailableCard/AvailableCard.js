308cbfa51b913948c17ea0eb513d7c739144b32f
ShareefRahat <imr4h47@gmail.com>
2022-05-14 20:13:52 +0600

    formatted date sent to server in available api to get available service after booking

--- src/components/AvailableCard/AvailableCard.js
+++ src/components/AvailableCard/AvailableCard.js
@@ -1,12 +1,19 @@
 import { format } from "date-fns";
-import React, { useState } from "react";
-import useServices from "../../hooks/useServices";
+import React, { useEffect, useState } from "react";
+
 import BookingCard from "../BookingCard/BookingCard";
 import BookingModal from "../BookingModal/BookingModal";
 
 const AvailableCard = ({ date }) => {
-  const [services] = useServices();
+  const [services, setServices] = useState();
   const [treatment, setTreatment] = useState(null);
+
+  const formattedDate = format(date, "PP");
+  useEffect(() => {
+    fetch(`http://localhost:5000/available?date=${formattedDate}`)
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, [formattedDate]);
 
   return (
     <div className="my-10">
--- src/hooks/useServices.js
+++ src/hooks/useServices.js
@@ -1,13 +1,16 @@
+import { format } from "date-fns";
 import { useEffect, useState } from "react";
 
 const useServices = () => {
   const [services, setServices] = useState();
+  const [date] = useState(new Date());
+  const formattedDate = format(date, "PP");
 
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch(`http://localhost:5000/available?${formattedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [formattedDate]);
   return [services, setServices];
 };
 

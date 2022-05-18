a21bd915ab4358243553f799c60a4c115be35b00
ShareefRahat <imr4h47@gmail.com>
2022-05-18 11:58:27 +0600

    manage doctor table added

--- src/pages/Dashboard/ManageDoctor.js
+++ src/pages/Dashboard/ManageDoctor.js
@@ -21,6 +21,33 @@ const ManageDoctor = () => {
         <h4 className="my-10 text-xl">Manage doctor </h4>
         <p>Total doctor: {doctors.length}</p>
       </section>
+      <section>
+        <div class="overflow-x-auto">
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>email</th>
+                <th>Specialty</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {doctors &&
+                doctors?.map((doctor, index) => (
+                  <tr key={doctor._id}>
+                    <th>{index + 1}</th>
+                    <td>{doctor.name}</td>
+                    <td>{doctor.email}</td>
+                    <td>{doctor.specialty}</td>
+                    <td>Action</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </>
   );
 };

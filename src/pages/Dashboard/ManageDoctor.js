262d6891522944c1fc5277da608226ef208b0faa
ShareefRahat <imr4h47@gmail.com>
2022-05-18 12:51:07 +0600

    delete funcitonality is added

--- src/pages/Dashboard/ManageDoctor.js
+++ src/pages/Dashboard/ManageDoctor.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { useQuery } from "react-query";
+import { toast } from "react-toastify";
 import Loading from "../../components/Loading/Loading";
 
 const ManageDoctor = () => {
-  const { data: doctors, isLoading } = useQuery("doctors", () =>
+  const {
+    data: doctors,
+    isLoading,
+    refetch,
+  } = useQuery("doctors", () =>
     fetch(`https://cavity-care.herokuapp.com/doctors`, {
       headers: {
         "content-type": "application/json",
@@ -15,6 +20,25 @@ const ManageDoctor = () => {
   if (isLoading) {
     return <Loading></Loading>;
   }
+
+  const handleDelete = (email) => {
+    fetch(`http://cavity-care.herokuapp.com/doctors/${email}}`, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount) {
+          toast.success(`Doctor ${email} info is deleted`);
+          refetch();
+        } else {
+          toast.error("Something went wrong!");
+        }
+      });
+  };
   return (
     <>
       <section>
@@ -26,7 +50,8 @@ const ManageDoctor = () => {
           <table class="table w-full">
             <thead>
               <tr>
-                <th></th>
+                <th>S/L</th>
+                <th>Image</th>
                 <th>Name</th>
                 <th>email</th>
                 <th>Specialty</th>
@@ -38,10 +63,27 @@ const ManageDoctor = () => {
                 doctors?.map((doctor, index) => (
                   <tr key={doctor._id}>
                     <th>{index + 1}</th>
+                    <td>
+                      <div class="avatar">
+                        <div class="w-12 rounded-full">
+                          <img
+                            src={doctor.img}
+                            alt="Tailwind-CSS-Avatar-component"
+                          />
+                        </div>
+                      </div>
+                    </td>
                     <td>{doctor.name}</td>
                     <td>{doctor.email}</td>
                     <td>{doctor.specialty}</td>
-                    <td>Action</td>
+                    <td>
+                      <button
+                        onClick={() => handleDelete(doctor.email)}
+                        className="btn btn-error btn-xs text-white"
+                      >
+                        Delete
+                      </button>
+                    </td>
                   </tr>
                 ))}
             </tbody>
--- src/pages/Dashboard/MyAppointment.js
+++ src/pages/Dashboard/MyAppointment.js
@@ -36,28 +36,34 @@ const MyAppointment = () => {
         <h4 className="text-xl">Total Appointment: {appointments.length}</h4>
       </section>
       <section>
-        <div class="overflow-x-auto">
-          <table class="table w-full">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Appointment Name</th>
-                <th>Date</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {appointments?.map((appointment, index) => (
-                <tr key={appointment._id}>
-                  <th>{index + 1}</th>
-                  <td>{appointment.treatment}</td>
-                  <td>{appointment.date}</td>
-                  <td>{appointment.slot}</td>
+        {!appointments ? (
+          <div>
+            <Loading></Loading>
+          </div>
+        ) : (
+          <div class="overflow-x-auto">
+            <table class="table w-full">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Appointment Name</th>
+                  <th>Date</th>
+                  <th>Time</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {appointments?.map((appointment, index) => (
+                  <tr key={appointment._id}>
+                    <th>{index + 1}</th>
+                    <td>{appointment.treatment}</td>
+                    <td>{appointment.date}</td>
+                    <td>{appointment.slot}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </section>
     </>
   );
